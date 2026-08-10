527068f27bec57052ea249a065d3c2deb9911627
Sindre Oftebro <osindreo@gmail.com>
2026-08-10 22:56:33 +0200

    galleri endre rekkefølgen

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const o=new IntersectionObserver(e=>e.forEach(x=>x.isIntersecting&&x.target.classList.add("show")));document.querySelectorAll(".reveal").forEach(x=>o.observe(x));
+
+// Tilfeldig rekkefølge i galleriet ved hver sidelasting/opplasting
+document.querySelectorAll(".idebank").forEach(gallery => {
+  const images = Array.from(gallery.children);
+
+  for (let i = images.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [images[i], images[j]] = [images[j], images[i]];
+  }
+
+  images.forEach(image => gallery.appendChild(image));
+});
+
